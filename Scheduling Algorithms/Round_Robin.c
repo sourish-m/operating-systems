1c7163b90eb2a43d65c9e0749ce22cc9c4307995
Carbon <samratroychowdhury3@gmail.com>
2022-04-05 14:23:12 +0530

    :)

--- Scheduling Algorithms/Round_Robin.c
+++ Scheduling Algorithms/Round_Robin.c
@@ -4,31 +4,30 @@
 #include <stdlib.h>
 void line(int);
 
-struct PCB
-{
+struct PCB {
 	int p, at, bt, wt, ta, rt;
 	bool visited;
-}a[10], t;
+} a[10], t;
 
-int main() { 
+int main() {
 	int i, j, c=0, idle=0, n, con_swt[20], d=0;
 	float WTsum=0, TAsum=0, WTavg, TAavg, util_time=0, util_perc;
 	char cswt_idle[40];
 	int quant, comp=0, q[10], front=0, rear=-1;
 //	clrscr();
-	
+
 	do {
 		printf("\n Enter number of processes: ");
 		scanf("%d", &n);
-		
+
 		if(n<1) {
 			system("cls");
 			printf("\n Number of processes must be > 0");
 		}
-	}while(n<1);
-	
+	} while(n<1);
+
 	printf("\n Enter Arrival time(AT) and Burst time(BT) for processes:\n");
-	for(i=0;i<n;i++) {
+	for(i=0; i<n; i++) {
 		a[i].p=i;
 		printf("\n Process %d", i);
 		line(11);
@@ -43,21 +42,21 @@ int main() {
 	}
 	printf(" Enter time quantum: ");
 	scanf("%d", &quant);
-	
+
 	system("cls");
 	printf("\n --------- Round Robin Scheduling ---------\n");
 	printf("\n Input:");
 	line(37);
 	printf(" Process   Arrival time   Burst Time");
 	line(37);
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
 		printf(" P%d            %d ms          %d ms\n", i, a[i].at, a[i].bt);
-		
+
 	printf("\n Time quantum: %d\n", quant);
-	
+
 //	sorting acc. to increasing arrival time
-	for(i=0;i<n;i++) {
-		for(j=i+1;j<n;j++) {
+	for(i=0; i<n; i++) {
+		for(j=i+1; j<n; j++) {
 			if(a[j].at<a[i].at) {
 				t=a[i];
 				a[i]=a[j];
@@ -65,14 +64,14 @@ int main() {
 			}
 		}
 	}
-	
+
 //	calculations
 	q[++rear]=0;
 	a[0].visited=true;
 	while(comp!=n) {
 		i=q[front];
 		front++;
-		
+
 		if(a[i].at>c) {
 			if(c) {
 				idle+=a[i].at-c;
@@ -82,34 +81,33 @@ int main() {
 			c=a[i].at;
 			cswt_idle[d]='o';
 		}
-		
+
 		if(a[i].rt>quant) {
-	    	con_swt[d++]=c;
-	        a[i].rt-=quant;
-	        c+=quant;
-	    }
-		else {
 			con_swt[d++]=c;
-		    c+=a[i].rt;
-		    a[i].rt=0;
-		    comp++;
-	        a[i].ta=c-a[i].at;
+			a[i].rt-=quant;
+			c+=quant;
+		} else {
+			con_swt[d++]=c;
+			c+=a[i].rt;
+			a[i].rt=0;
+			comp++;
+			a[i].ta=c-a[i].at;
 			a[i].wt=a[i].ta-a[i].bt;
 			WTsum+=a[i].wt;
 			TAsum+=a[i].ta;
 		}
-	    
-	    for(j=1; j<n; j++) {
-	    	if(a[j].rt>0 && a[j].at<=c && !a[j].visited) {
-	    		q[++rear]=j;
-	    		a[j].visited=true;
+
+		for(j=1; j<n; j++) {
+			if(a[j].rt>0 && a[j].at<=c && !a[j].visited) {
+				q[++rear]=j;
+				a[j].visited=true;
 			}
 		}
-		
+
 		if(a[i].rt>0) {
 			q[++rear]=i;
 		}
-		
+
 		if(front>rear) {
 			for(i=1; i<n; i++) {
 				if(a[i].rt>0) {
@@ -124,10 +122,10 @@ int main() {
 	WTavg=WTsum/n;
 	TAavg=TAsum/n;
 	util_perc=util_time/c*100;
-	
-	
-	for(i=0;i<n;i++) {
-		for(j=i+1;j<n;j++) {
+
+
+	for(i=0; i<n; i++) {
+		for(j=i+1; j<n; j++) {
 			if(a[j].p<a[i].p) {
 				t=a[i];
 				a[i]=a[j];
@@ -139,16 +137,16 @@ int main() {
 	line(42);
 	printf(" Process   Waiting time   Turnaround Time");
 	line(42);
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
 		printf(" P%d            %d ms            %d ms\n", a[i].p, a[i].wt, a[i].ta);
-	
+
 	printf("\n\n Average Waiting Time: %.3f ms\n", WTavg);
 	printf("\n Average Turnaround Time: %.3f ms\n", TAavg);
 	printf("\n CPU Utilization Time: %.3f ms\n", util_time);
 	printf("\n CPU Utilization Percentage: %.3f %%\n", util_perc);
 	printf("\n CPU Idle Time: %.3f ms\n", (float)idle);
 	printf("\n Context switch points:");
-	for(i=0;i<d;i++) {
+	for(i=0; i<d; i++) {
 		printf("\n -> %d ms", con_swt[i]);
 		if(i==0 || cswt_idle[i]=='o')
 			printf(" | OS -> Process");
@@ -158,7 +156,7 @@ int main() {
 			printf(" | Process -> OS -> Process");
 	}
 	printf("\n Mode switch points:");
-	for(i=0;i<d;i++) {
+	for(i=0; i<d; i++) {
 		printf("\n -> %d ms", con_swt[i]);
 		if(i==0 || cswt_idle[i]=='o')
 			printf(" | Kernel -> User");
@@ -167,14 +165,15 @@ int main() {
 		else
 			printf(" | User -> Kernel -> User");
 	}
-	
+
 	getch();
 }
 
-void line(int x) {
+void line(int x)
+{
 	int i;
 	printf("\n");
-	for(i=0;i<x;i++)
+	for(i=0; i<x; i++)
 		printf("-");
 	printf("\n");
 }
